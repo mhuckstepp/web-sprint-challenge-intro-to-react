8321a938abefe73cfd25c6341a1c7b7091c4c337
Max Huckstepp <mhuckstepp@gmail.com>
2021-02-19 09:02:40 -0800

    MVP

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,18 @@
-// Write your Character component here
+import React from "react";
+import styled from "styled-components";
+
+const Character = (props) => {
+  const { characters } = props;
+
+  return (
+    <div>
+      {characters.map((character) => (
+        <div className="card" key={character.height}>
+          {character.name}{" "}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Character;
